8f626f6fce64c19c05b5608c51895d6a7a292edb
RAYINI NAGASHIVA <121884111+Nagashiva123@users.noreply.github.com>
2023-10-09 09:57:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import SnippetManager from './SnippetManager';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <SnippetManager />
+  </React.StrictMode>
+);
